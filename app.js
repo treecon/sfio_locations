e4a9b21b87ff6832d99f981cbbfa4028f54ad7e3
treecon <kon3dos@gmail.com>
2022-03-09 10:42:26 +0200

    weighted

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+import os from 'os';
 import fs from 'fs';
 import { Generation } from './generation.js';
 import config from './config.js';
 
+const timest = Date.now();
+
 console.log(`started at ${new Date().toLocaleTimeString()}`);
 
-const demandPoints = JSON.parse(fs.readFileSync(`./data/${config.demandPointsGeoJson}`));
+let demandPoints = JSON.parse(fs.readFileSync(`./data/${config.demandPointsGeoJson}`));
+demandPoints = {
+    ...demandPoints,
+    features: demandPoints.features.filter(x => x.properties.population),
+};
 const gridPoints = JSON.parse(fs.readFileSync(`./data/${config.gridPointsGeoJson}`));
 const parameters = {
     populationNum: config.populationNum,
@@ -22,6 +29,7 @@ const nextGeneration = (generationIndex, population, demandPoints, gridPoints, p
     let generation = new Generation(population, demandPoints, gridPoints, parameters);
 
     console.log(`generation ${generationIndex}: ${generation.best.pMedianLength}, ${JSON.stringify(generation.best)}`);
+    fs.appendFileSync(`./data/results_weighted_${timest}`, `${new Date().toLocaleTimeString()}, ${generationIndex}, ${JSON.stringify(generation.best)}${os.EOL}`);
 
     if (generationIndex > config.numberOfGenerations) {
         console.log(generation.best);
--- generation.js
+++ generation.js
@@ -198,7 +198,7 @@ function Generation(population, demandPoints, gridPoints, parameters) {
                 }
             }
             // lines.push(turf.lineString([pointA.geometry.coordinates, connectedFacility.geometry.coordinates]));
-            pMedianLength += dist;
+            pMedianLength += dist*pointA.properties.population; // WEIGHTED
         }
         let score = 1/pMedianLength;
         score = Math.pow(score, parameters.oddsParam);
